e0d491c2b9e527058ae09a418a3df74bd6a0e91e
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-04-27 15:24:35 -0600

    add loop and ui logic in place

--- src/api-service.js
+++ src/api-service.js
@@ -1,6 +1,6 @@
 export default class ClevelandArt {
   static getArt(department) {
-    return fetch(`https://openaccess-api.clevelandart.org/api/artworks/?department=${department}&limit=30&has_image=1`)
+    return fetch(`https://openaccess-api.clevelandart.org/api/artworks/?department=${department}&has_image=1&limit=30`)
       .then(function (response) {
         if (!response.ok) {
           throw Error(response.statusText);
--- src/main.js
+++ src/main.js
@@ -4,31 +4,51 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './css/styles.css'
 import ClevelandArt from './api-service.js'
 
-function showData(response) {
-  console.log(response)
-  const firstElement = response[0]
-  console.log(firstElement)
-  let html = `<div class="col-md-3 p-2 card">`
-  html += `<h2 class="title">${firstElement.title}</h2>`
-  html += `<h4 class="creation_date">Created c. ${firstElement.creation_date}</h4>`
-  html += `<h6 class="culture">${firstElement.culture}</h6>`
-  html += `<p class="fun_fact">${firstElement.fun_fact}</p>`
-  html += `<div class="images"><img width="300px;" src="${firstElement.images.web.url}"></div>`
-  html += `<p class="wall_description">${firstElement.wall_description}</p>`
+function showData(artDataArray) {
+  let html = `<div class="row">`
+  artDataArray.forEach(function (artData) {
+    html += `<div class="col-md-3 p-2 card">`
+    html += `<h2 class="title">${artData.title}</h2>`
+    html += `<h4 class="creation_date">Created c. ${artData.creation_date}</h4>`
+    html += `<h6 class="culture">${artData.culture}</h6>`
+    html += `<p class="fun_fact">${artData.fun_fact}</p>`
+    html += `<div class="images"><img width="300px;" src="${artData.images.web.url}"></div>`
+    html += `<p class="wall_description">${artData.wall_description}</p>`
+    html += `</div>`
+  })
   html += `</div>`
-
   $(".category-results").html(html)
 }
 
+function cacheData(dataUrl, data) {
+  localStorage.setItem(dataUrl, JSON.stringify(data))
+}
+
+function retrieveCache(dataUrl) {
+  return JSON.parse(localStorage.getItem(dataUrl))
+}
+
+function showFlashcardsPage() {
+  const cache = retrieveCache()
+  console.log(cache)
+  // TODO
+}
 
 $("button").click(function (event) {
   const department = event.currentTarget.id.trim()
+  if (department === "flashcards-button") {
+    return showFlashcardsPage()
+  }
   ClevelandArt.getArt(department)
     .then(function (response) {
       if (response instanceof Error || (response && response.message && response.stack)) {
         return alert(`you have encountered an error ${response.statusText}`)
       }
+      console.log(response)
       // now we know we don't have an error...
+      // save the data to local storage
+      cacheData(department, response.data)
+      // show the data on screen
       showData(response.data);
     })
 })
